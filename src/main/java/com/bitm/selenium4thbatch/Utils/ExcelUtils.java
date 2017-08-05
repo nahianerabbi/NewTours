89ee1e54e27e3bc4dc5d080ffdd127dcd1bb1bde
nahianerabbi <nahianrabbi2@gmail.com>
2017-08-05 12:26:26 +0600

    excel utils and dataprovider update

--- src/main/java/com/bitm/selenium4thbatch/Utils/ExcelUtils.java
+++ src/main/java/com/bitm/selenium4thbatch/Utils/ExcelUtils.java
@@ -3,10 +3,17 @@ package com.bitm.selenium4thbatch.Utils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.bitm.selenium4thbatch.DTO.LoginDto;
 
 public class ExcelUtils {
 
@@ -19,5 +26,46 @@ public class ExcelUtils {
 		Sheet sheet = (Sheet) workbook.getSheetAt(sheetNo);
 		return sheet;
 	}
+	
+	public static List<LoginDto> getLoginData() throws IOException{
+		List<LoginDto> logins=new ArrayList<LoginDto>();
+		
+		//login is the first sheet in excel so getSheet parameter set to 0
+		Iterator<Row> iterator = ExcelUtils.getSheet(0).iterator();
+        while (iterator.hasNext()) {
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+            LoginDto logindata=new LoginDto();
+            byte cellCounter=0;
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cellCounter) {
+				case 0:
+					logindata.setUsername(cell.getStringCellValue());
+					cellCounter++;
+					break;
+				case 1:
+					logindata.setPassword(cell.getStringCellValue());
+					break;
+				default:
+					break;
+				}               
+                 
+            }
+            logins.add(logindata);
+        }
+        close();
+		return logins;
+}
+
+	private static void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+		workbook.close();
+		inputStream.close();
+		
+	}
+	
+	
 
 }
